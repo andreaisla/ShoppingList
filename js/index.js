5b3f8178a9e29a7dc76f93787e728aaaab9c8c61
aislaagui <87794337+aislaagui@users.noreply.github.com>
2022-12-19 10:00:55 +1100

    add table

--- js/index.js
+++ js/index.js
@@ -18,20 +18,60 @@ const items = [
   },
 ];
 
-const container = document.getElementById("product_container");
+const cart = {
 
-// For each of the items in the array, we append a new <div> element,
-// with all the fields, into the main container
-for (let i = 0; i < items.length; i++) {
-  const item = items[i];
-  container.innerHTML += `<div class="col-sm-4">
-    <div style="background-color:#ADF" class="p-2">
-      <h6>${item.name}</h6>
-      <p>Price: $${item.price} AUD</p>
-      <img src="${item.image}" class="w-100 mb-2">
-      <div class="d-grid gap-2">
-        <button class="btn btn-success">Add to cart</button>
-      </div>
-    </div>
-  </div>`;
 }
+
+function displayCatalog() {
+  const container = document.getElementById("product_container");
+
+  // For each of the items in the array, we append a new <div> element,
+  // with all the fields, into the main container
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    container.innerHTML += `<div class="col-sm-4">
+      <div style="background-color:#ADF" class="p-2">
+        <h6>${item.name}</h6>
+        <p>Price: $${item.price} AUD</p>
+        <img src="${item.image}" class="w-100 mb-2">
+        <div class="d-grid gap-2">
+          <button class="btn btn-success add-to-cart" data-productname="${item.name}">Add to cart</button>
+        </div>
+      </div>
+    </div>`;
+  }
+
+  for (button of document.getElementsByClassName('add-to-cart')) {
+    button.addEventListener('click', function (event) {
+      addToCart(event.target.dataset.productname);
+    })
+  }
+
+}
+
+function addToCart(name) {
+  if (cart[name] === undefined) {
+    cart[name] = 1;
+  } else {
+    cart[name]++;
+  }
+  displayCart();
+}
+
+function displayCart() {
+  const cartContainer = document.getElementById("cart_container");
+  cartContainer.innerHTML = '';
+  // For each of the items in the array, we append a new <div> element,
+  // with all the fields, into the main container
+  for (let productName in cart) {
+    const productQuantity = cart[productName];
+    cartContainer.innerHTML += `<tr>
+      <td>
+        ${productName}
+      </td>
+      <td>${productQuantity}</td>
+    </tr>`;
+  }
+}
+
+displayCatalog();
